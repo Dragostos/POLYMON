daa104e87e6e23e2c8647f155f82cc8e68c0c76b
Dragostos <131541709+Dragostos@users.noreply.github.com>
2023-05-15 15:20:45 -0500

    Moves
    moved moves to another file to organize it

--- polymon.py
+++ polymon.py
@@ -87,40 +87,7 @@ p_damage = 0
 e_damage = 0
 bar_change = 0
 
-move_list = {
-    'Poly Tackle' : {
-        'Damage': 2,
-        'Power': 35,
-        'Accuracy': 90,
-        'Name': 'Poly Tackle',
-        'Type': 'Physical'
-    },
-    'Square Fury': {
-        'Chance': 10,
-        'Damage': 2,
-        'Power': 35,
-        'Accuracy': 90,
-        'Name': 'Square Fury',
 
-    },
-    'Polyscare': {
-        'Debuff': 1.1,
-        'Name': 'Polyscare'
-    },
-    'Ridicule': {
-        'Attack Buff Increase': 1.1,
-        'Defense Debuff Decrease': 1.1,
-        'Name': 'Ridicule'
-    }}
-
-moves = {
-    'move 1' : move_list['Poly Tackle'],
-    'move 2' : move_list['Square Fury'],
-    'move 3' : move_list['Polyscare'],
-    'move 4' : move_list['Ridicule']
-}
-
-move_options = ['Poly Tackle', 'Square Fury', 'Polyscare', 'Ridicule']
 
 
 class Entity:
